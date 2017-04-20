690ac19835d41c7bbe3eab979b9f4b521b99e013
Mikhail Garbuzov <madmxg@gmail.com>
2017-04-20 15:30:08 +0300

    injections

--- src/injections.js
+++ src/injections.js
@@ -1,6 +1,8 @@
-/* global document:true */
+/* global window:true document:true */
 
 'use strict';
+
+import helper from 'bet-helper';
 
 function tagInjector (c) {
   return (() => {
@@ -12,28 +14,68 @@ function tagInjector (c) {
   });
 }
 
+function evalInjector (c) {
+  return (() => {
+    const m = `\n${c}\n`;
+    new Function('W, D', m)(window, document);
+  });
+}
+
+function apiInjector (c) {
+  return (() => {
+    const m = `\n${c}\n`;
+    new Function('W, D, A, M', m)(window, document, {}, {});
+  });
+}
+
+function linkInjector (l) {
+  return (() => {
+    const s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.charset = 'utf-8';
+    s.src = l;
+    document.getElementsByTagName('head')[0].appendChild(s);
+  });
+}
+
 function getInjector (m) {
-  if (m.c) {
-    return tagInjector(m.c);
+  switch (m.i) {
+    case 1:
+      return evalInjector(m.c);
+    case 2:
+      return apiInjector(m.c);
+    case 3:
+      return linkInjector(m.l);
+    case 0:
+    default:
+      return tagInjector(m.c);
   }
 }
 
 export function injectRandom (m) {
   const injector = getInjector(m);
-  injector();
+  const [min, max] = `${m.r}`.split('.');
+  const rnd = helper.random(min, max) * 1000;
+  console.log('injectRandom', m.r);
+  console.log('rnd', rnd);
+
+  helper.contentLoaded(window, () => setTimeout(injector, rnd));
 }
 
 export function injectWithDelay (m) {
+  console.log('injectWithDelay', m.r);
   const injector = getInjector(m);
-  injector();
+  helper.contentLoaded(window, () => setTimeout(injector, m.r * 1000));
 }
 
 export function injectImmediately (m) {
+  console.log('injectImmediately', m.r);
   const injector = getInjector(m);
   injector();
 }
 
 export function injectDocumentReady (m) {
+  console.log('injectDocumentReady', m.r);
   const injector = getInjector(m);
-  injector();
+  helper.contentLoaded(window, injector);
 }
